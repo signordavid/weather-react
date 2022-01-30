94a630a5fe0f38b1d036cb641515b8d011543b0d
signordavid <dafapunkv@gmail.com>
2022-01-30 17:51:45 +0100

    2

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -3,13 +3,13 @@ import React from 'react';
 const WeatherForm = props => (
     <div className="card card-body">
         <form onSubmit={props.getWeather}>
-            <div className="form-group">
+            <div className="form-group m-2">
                 <input type="text" name="city" placeholder="Your City Name" className="form-control" autoFocus/>
             </div>
-            <div className="form-group">
+            <div className="form-group m-2">
                 <input type="text" name="country" placeholder="Your Country Name" className="form-control" />
             </div>
-            <button className="btn btn-success btn-block">
+            <button className="btn btn-success btn-block m-2">
                 Get Weather
             </button>
         </form>
